070f95157eda560045dd3f65bf3b32c6f9e9cad5
Nitish Ghormade <dadughormade@gmail.com>
2025-06-22 10:49:07 +0530

    XSS 2nd Protection done

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from urllib.parse import unquote, quote
 app = Flask(__name__)
 
 def Check_SQLi(query):
+    if not query:
+        return False
     SQLi_patterns = [
         "'", '"', "--", "#", ";", " ", "exec", "select", "from", "where", "and", "or", "not", "in", "union"
     ]
@@ -18,13 +20,37 @@ def Check_XSS(query):
     if not query:
         return False
     xss_tags = [
-        "<script", "<img", "<iframe", "<svg", "<object", "<embed", "<link", "<style"
+        "<script", "<iframe", "<svg", "<object", "<embed", "<link", "<style", "javascript"
     ]
+    # Blocked tags above; now handle specific attributes for other tags
+    dangerous_tag_attrs = {
+        "<img": ["onerror", "onload", "src", "style"],
+        "<a": ["href", "onclick", "onmouseover"],
+        "<body": ["onload", "onerror", "onresize"],
+        "<video": ["onerror", "onload", "src"],
+        "<audio": ["onerror", "onload", "src"],
+        "<form": ["action", "onsubmit"],
+        "<input": ["onfocus", "onblur", "onchange", "oninput", "value", "autofocus"],
+        "<button": ["onclick", "onfocus", "autofocus"],
+        "<marquee": ["onstart", "onfinish"],
+        "<div": ["onclick", "onmouseover", "onmouseenter", "onmouseleave"],
+        "<span": ["onclick", "onmouseover", "onmouseenter", "onmouseleave"],
+        "<textarea": ["onfocus", "onblur", "onchange", "oninput", "autofocus"],
+        "<select": ["onfocus", "onblur", "onchange", "autofocus"]
+    }
+    lower_query = query.lower()
+    # Step 1: Block if any of the main XSS tags or their encodings are present
     for tag in xss_tags:
-        if tag in query.lower():
+        if tag in lower_query:
             return True
-        elif quote(tag) in query.lower():
+        elif quote(tag) in lower_query:
             return True
+    # Step 2: For each tag in dangerous_tag_attrs, if tag and any dangerous attribute are present, block
+    for tag, attrs in dangerous_tag_attrs.items():
+        if tag in lower_query or quote(tag) in lower_query:
+            for attr in attrs:
+                if attr in lower_query or quote(attr) in lower_query:
+                    return True
     return False
 
 @app.before_request
